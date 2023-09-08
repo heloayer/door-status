6c6f82cf763abb6cbb0cd4697dfc47e5f3fc54aa
heloayer <us.gogle11@gmail.com>
2023-09-08 18:35:35 +0600

    negative temp fixes, small changes

--- cmd/main.go
+++ cmd/main.go
@@ -17,20 +17,27 @@ func Decode(hexStr string) (*Result, error) {
 		return nil, err
 	}
 
-	// fmt.Println(bytes)
 	result := &Result{}
 
 	for i := 0; i < len(bytes); i++ {
-		temperatureBytes := bytes[2:4] // для подсчета значения температуры
-		temperatureInt := int(temperatureBytes[0])<<8 + int(temperatureBytes[1])
+
 		switch bytes[i] {
 		case 0x03: // Температура
 
 			if bytes[i+1] == 0x67 { // если тип канала Temperature соответствует
-				if bytes[i+2] == 0xFF { // отрицательное значение температуры
-
-					temperatureInt = ^temperatureInt + 1
-					result.Temperature = float64((temperatureInt ^ 0xFFFF + 1))
+				if bytes[i+2] == 0xFF { // если отрицательная температура
+					// если длина байта последнего значения отрицательной температуры больше 1, то форма FFFFFF
+					if len(string(bytes[i+4])) != 1 {
+						triple_F_type := bytes[3:5]
+						negative_temp := int(triple_F_type[0])<<8 + int(triple_F_type[1])
+						negative_temp = ^negative_temp + 1
+						result.Temperature = float64((negative_temp ^ 0xFFFF + 1))
+					} else { // форма FFFF
+						two_F_type := bytes[2:4]
+						negative_temp := int(two_F_type[0])<<8 + int(two_F_type[1])
+						negative_temp = ^negative_temp + 1
+						result.Temperature = float64((negative_temp ^ 0xFFFF + 1))
+					}
 				} else { // положительное значение температуры
 					result.Temperature = float64(int16(bytes[3])<<8|int16(bytes[2])) / 10.0
 				}
@@ -41,7 +48,7 @@ func Decode(hexStr string) (*Result, error) {
 		case 0x04: // Влажность
 
 			if bytes[i+1] == 0x68 { // если тип канала Humidity соответствует
-				result.Humidity = float64(int(bytes[6])) / 2.0 // Формула: Humidity = 130 / 2.0
+				result.Humidity = float64(int(bytes[i+2])) / 2.0 // Формула: байт Humidity / 2.0
 			} else {
 				fmt.Println("ERROR: Wrong channel type for Humidity")
 			}
@@ -63,8 +70,8 @@ func Decode(hexStr string) (*Result, error) {
 
 func main() {
 
-	hexStr := "0367F600046882060001" //
-	// hexStr := "0367FFE1046882060001" // hex код с отрицательной температурой (-31)
+	// hexStr := "0367F600046882060001" // положительная (24.6 С)
+	hexStr := "0367FFFFE1046882060001" // hex код с отрицательной температурой (-31)
 	result, err := Decode(hexStr)
 	if err != nil {
 		fmt.Println("Error:", err)
